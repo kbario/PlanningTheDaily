3142735d6de0a01e44c80a250d91f7171f50b793
kbario <kylebario1@gmail.com>
2022-01-13 13:16:48 +0800

    intial js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Creating Element Vars
+
+var dateEl = document.getElementById('currentDay');
+
+var today = moment().format("dddd, MMM Do, YYYY")
+
+dateEl.textContent = today
+
+// get the time until next day and set as time to update the day?
+
+
